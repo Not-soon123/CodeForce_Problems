912d136ad8d840ee04b52f8dad20115fe9a302ed
Natan Tibebu <natantibebu111@gmail.com>
2026-04-22 07:09:18 +0300

    Adding some Clearity to the input

--- WayToLongWord.py
+++ WayToLongWord.py
@@ -1,10 +1,11 @@
-number = int(input())
+number = int(input("Enter the number of words: "))
 
 for i in range(number):
-    work = input()
-    workLeangth = len(work)
-    if workLeangth > 10:
-        print(work[0] + str(workLeangth - 2)+ work[-1] )
+    
+    work = input("Enter the word: ")
+    workLength = len(work)
+    if workLength > 10:
+        print(work[0] + str(workLength - 2)+ work[-1] )
     else:
         print(work)
 
