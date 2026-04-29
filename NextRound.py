1f5376a3bd7802e14e5302ecd2ae893bb811befa
Natan Tibebu <natantibebu111@gmail.com>
2026-04-29 22:14:38 +0300

    Finally Works I solved by correcting the edge

--- NextRound.py
+++ NextRound.py
@@ -2,6 +2,7 @@ n,k = map(int,(input().split()))
 
 list1 = list(map(int,(input().split())))
 list1.sort(reverse = True) # to make the list in dicident order
+print(list1)
 count = 0
 # count the numbers that are greater than and equal to the index"k" in list1
 #print(list1)
@@ -9,7 +10,8 @@ if list1[k-1] != 0:
     for i in list1:
         if i >= list1[k-1]:
             count+=1
-else:
-    count = 0       
-
+if list1[k-1] == 0:
+         for i in list1:
+            if i > 0:
+                count+=1 
 print(count)
