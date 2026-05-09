9b3ff9a1bc47b07967c46033881fc9085be1588f
Natan Tibebu <natantibebu111@gmail.com>
2026-05-09 14:10:09 +0300

    Accepted

--- Elephant.py
+++ Elephant.py
@@ -2,14 +2,29 @@ LocationOfFriend = int(input())
 stepAtOnce = [1,2,3,4,5]
 step = 0
 if 1 < LocationOfFriend < 1000000:
-    i = 4
-    while LocationOfFriend >= stepAtOnce[i]:
-          LocationOfFriend -= stepAtOnce[i]
-          step += 1
+    while LocationOfFriend > 0:
 
-          if LocationOfFriend < stepAtOnce[i]:
-               while LocationOfFriend <= stepAtOnce[i]:
-                     i -= 1
-print(step)
+            if LocationOfFriend >= stepAtOnce[4]:
+                  LocationOfFriend -= stepAtOnce[4]
+                  step += 1
+
+            elif LocationOfFriend >= stepAtOnce[3]:
+                  LocationOfFriend -= stepAtOnce[3]
+                  step += 1
+                  
+            elif LocationOfFriend >= stepAtOnce[2]:
+                  LocationOfFriend -= stepAtOnce[2]
+                  step += 1
+
+            elif LocationOfFriend >= stepAtOnce[1]:
+                  LocationOfFriend -= stepAtOnce[1]
+                  step += 1
+
+            else :
+                  LocationOfFriend -= stepAtOnce[0]
+                  step += 1
+                  
+            
+    print(step)
                
 
