95e6dfa9f68f57aa5cc23f173c7c965330fc01bf
Natan Tibebu <natantibebu111@gmail.com>
2026-05-19 09:58:47 +0300

    Accepted

---
+++ Football.py
@@ -0,0 +1,8 @@
+number_of_players_in_binary = "00100110111111101"
+
+if "1111111" in number_of_players_in_binary or "0000000" in number_of_players_in_binary:
+    print("YES")
+else:
+    print("NO")
+
+  
